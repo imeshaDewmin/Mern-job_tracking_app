bc519e149fdf2efc9c4dc559759d20ddbd07696b
imeshaDewmin <dewmin2001@gmail.com>
2024-04-12 19:58:52 +0530

    Setting up the server

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('Server is working');
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from "express";
+const app = express();
+import morgan from "morgan";
+
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+app.post('/', (req, res) => {
+    console.log(req);
+    res.json({ message: 'data recieved', data: req.body });
+})
+
+const port = process.env.PORT || 5100;
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}....`);
+});
+
