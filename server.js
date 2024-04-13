a000469db5874f251d5f9d941e12deacf92786ea
imeshaDewmin <dewmin2001@gmail.com>
2024-04-13 14:48:40 +0530

    Connect to MongoDB

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
+import mongoose from 'mongoose';
 
 //Routes
 import jobRouter from './routes/jobRouter.js';
@@ -27,7 +28,17 @@ app.use((err, req, res, next) => {
 })
 
 const port = process.env.PORT || 5100;
+
+try{
+await mongoose.connect(process.env.MONGO_URL)
 app.listen(port, () => {
   console.log(`server running on PORT ${port}....`);
 });
+
+}catch(error){
+console.log(error);
+process.exit(1);
+}
+
+
 
