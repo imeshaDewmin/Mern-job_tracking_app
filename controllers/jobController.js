752e9d513c86ea4338545abc3034abfad6d2a524
imeshaDewmin <dewmin2001@gmail.com>
2024-04-17 11:52:19 +0530

    Add custom errors and error handler middleware

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,7 @@
 import 'express-async-errors';
 import { StatusCodes } from 'http-status-codes';
 import Job from '../models/JobModel.js';
+import { NotFoundError } from '../errors/customErrors.js';
 
 export const getAllJobs = async (req, res) => {
     const jobs = await Job.find({})
@@ -16,9 +17,7 @@ export const getSingleJob = async (req, res) => {
     const { id } = req.params;
 
     const job = await Job.findById(id)
-    if (!job) {
-        return res.status(404).json({ msg: `no job with id ${id}` });
-    }
+    if (!job) throw new NotFoundError(`no job with id ${id}`)
     res.status(StatusCodes.OK).json({ job })
 };
 
@@ -28,18 +27,14 @@ export const updateJob = async (req, res) => {
     const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
         new: true
     })
-    if (!updatedJob) {
-        return res.status(404).json({ msg: `no job with id ${id}` });
-    }
-    res.status(StatusCodes.OK).json({ msg: 'job modified', job:updatedJob })
+    if (!updatedJob) throw new NotFoundError(`no job with id ${id}`);
+    res.status(StatusCodes.OK).json({ msg: 'job modified', job: updatedJob })
 };
 
 export const deleteJob = async (req, res) => {
 
     const { id } = req.params;
     const removedJob = await Job.findByIdAndDelete(id)
-    if (!removedJob) {
-        return res.status(404).json({ msg: `no job with id ${id}` });
-    }
+    if (!removedJob) throw new NotFoundError(`no job with id ${id}`);
     res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob });
 };
--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const app = express();
 import morgan from "morgan";
 import mongoose from 'mongoose';
 
-//Routes
+//Routers
 import jobRouter from './routes/jobRouter.js';
+
+//Middleware
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 
 
 if (process.env.NODE_ENV === 'development') {
@@ -22,10 +25,7 @@ app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
 });
 
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ msg: 'Something went wrong' });
-})
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5100;
 
