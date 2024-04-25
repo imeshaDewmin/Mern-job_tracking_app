7a5f8e5fa4a80aa088a2f67d7d7e2f7b868f23c7
imeshaDewmin <dewmin2001@gmail.com>
2024-04-25 19:06:50 +0530

    Add get application stats for the admin

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-import { UnauthenticatedError } from "../errors/customErrors.js";
+import { UnauthorizedError } from "../errors/customErrors.js";
 import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = (req, res, next) => {
@@ -11,7 +11,15 @@ export const authenticateUser = (req, res, next) => {
     } catch (error) {
         throw new UnauthenticatedError('authentication invalid');
     }
-
-
 };
+
+export const authorizePermissions = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            throw new UnauthorizedError('Unauthorized to access this route')
+        }
+        next()
+    }
+
+}
 
