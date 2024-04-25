2d233854b92808725f0894e06c84af19a4d1c63b
imeshaDewmin <dewmin2001@gmail.com>
2024-04-25 13:15:51 +0530

    Add user routes and controller

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import cookieParser from 'cookie-parser';
 //Routers
 import jobRouter from './routes/jobRouter.js';
 import authRouter from './routes/authRouter.js';
+import userRouter from './routes/userRouter.js';
 
 //Middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
@@ -23,7 +24,9 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
+app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
+
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
