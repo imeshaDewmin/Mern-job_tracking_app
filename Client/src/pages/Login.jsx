a6c7793932c23cffd259027812ccc2f4cc5d4ff1
imeshaDewmin <dewmin2001@gmail.com>
2024-04-26 17:08:43 +0530

    Complete login page functionality

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,17 +1,36 @@
-import { Link } from "react-router-dom";
-import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
-import { Logo,FormRow } from "../components";
+import { Link, Form, redirect, useNavigation } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+  try {
+    await customFetch.post('/auth/login', data)
+    toast.success('Login success')
+    return redirect('/dashboard')
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error;
+  }
+
+}
 
 const Login = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
   return (
     <Wrapper>
-      <form className='form'>
-        <Logo/>
+      <Form method='post' className='form'>
+        <Logo />
         <h4>Login</h4>
-        <FormRow type='email' name='email' defaultValue=''/>
-        <FormRow type='password' name='password' defaultValue=''/>
-        <button type="submit" className="btn btn-block">
-          Submit
+        <FormRow type='email' name='email' defaultValue='' />
+        <FormRow type='password' name='password' defaultValue='' />
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? 'submitting' : 'submit'}
         </button>
         <button type="button" className="btn btn-block">
           Explore the app
@@ -21,7 +40,7 @@ const Login = () => {
             Register
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 }
