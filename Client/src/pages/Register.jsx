fbf295b6210b0f96b714baeed3cb219e816b8eeb
imeshaDewmin <dewmin2001@gmail.com>
2024-04-26 12:09:17 +0530

    Complete register page functionality

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -1,28 +1,48 @@
-import { Link } from "react-router-dom";
+import { Form, redirect, useNavigation, Link } from 'react-router-dom';
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
-import { Logo,FormRow } from "../components";
+import { Logo, FormRow } from "../components";
+import customFetch from '../utils/customFetch';
+import { toast } from 'react-toastify';
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post('/auth/register', data);
+    toast.success('Registration successful')
+    return redirect('/login');
+
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error
+  }
+};
 
 const Register = () => {
+  const navigation = useNavigation()
+  console.log(navigation);
+  const isSubmitting = navigation.state === 'submitting'
   return (
     <Wrapper>
-      <form className="form">
+      <Form method='post' className="form">
         <Logo />
         <h4>Register</h4>
-       <FormRow type ='text' name='name' defaulValue=''/>
-       <FormRow type ='text' name='lastName' labelText='last name'defaulValue=''/>
-       <FormRow type ='text' name='location' defaulValue=''/>
-       <FormRow type ='email' name='email' defaulValue=''/>
-       <FormRow type ='password' name='password' defaulValue=''/>
+        <FormRow type='text' name='name' defaultValue='' />
+        <FormRow type='text' name='lastName' labelText='last name' defaultValue='' />
+        <FormRow type='text' name='location' defaultValue='' />
+        <FormRow type='email' name='email' defaultValue='' />
+        <FormRow type='password' name='password' defaultValue='' />
 
-        <button type="submit" className="btn btn-block">
-          Submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? 'submitting...' : 'submit'}
         </button>
         <p>Already a member?
           <Link to='/login' className="member-btn">
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 }
