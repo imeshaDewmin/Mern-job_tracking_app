a6be6cd81723ea86c6b5b46a4c71d75e328d435a
imeshaDewmin <dewmin2001@gmail.com>
2024-05-05 18:13:47 +0530

    Stats Page setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -18,11 +18,13 @@ import { action as loginAction } from "./pages/Login";
 import { action as createJobAction } from "./pages/AddJob";
 import { action as editJobAction } from "./pages/EditJob";
 import { action as deleteJobAction } from "./pages/DeleteJob";
+import { action as profileAction } from "./pages/Profile";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
 import { loader as adminLoader } from "./pages/Admin";
-import { action as profileAction } from "./pages/Profile";
+import { loader as statsLoader } from "./pages/Stats";
+
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
@@ -64,7 +66,8 @@ const router = createBrowserRouter([
           },
           {
             path: 'stats',
-            element: <Stats />
+            element: <Stats />,
+            loader: statsLoader,
           },
           {
             path: 'all-jobs',
