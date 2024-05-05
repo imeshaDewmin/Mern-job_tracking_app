db9df5c2f513bc47f4b0d220fe96b7816c27de6a
imeshaDewmin <dewmin2001@gmail.com>
2024-05-05 18:54:40 +0530

    Complete charts container

--- Client/src/components/ChartsContainer.jsx
+++ Client/src/components/ChartsContainer.jsx
@@ -1,6 +1,19 @@
-const ChartsContainer = () => {
+import { useState } from 'react';
+
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true);
   return (
-    <h1>ChartsContainer</h1>
+    <Wrapper>
+      <h4>Monthly Applications </h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
   )
 }
-export default ChartsContainer
+export default ChartsContainer;
