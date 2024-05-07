5fc016d20d4d013ca80d6445c0c743abe556b9b7
imeshaDewmin <dewmin2001@gmail.com>
2024-05-07 13:44:28 +0530

    Setup pagination logic

--- Client/src/components/PageBtnContainer.jsx
+++ Client/src/components/PageBtnContainer.jsx
@@ -7,19 +7,39 @@ const PageBtnContainer = () => {
     const { data: { numOfPages, currentPage } } = useAllJobsContext()
     const pages = Array.from({ length: numOfPages }, (_, index) => {
         return index + 1
-    })
+    });
+
+    const { search, pathname } = useLocation()
+    const navigate = useNavigate()
+
+    const handlePageChange = (pageNumber) => {
+        const searchParams = new URLSearchParams(search)
+        searchParams.set('page', pageNumber);
+        navigate(`${pathname}?${searchParams.toString()}`);
+    }
+
     return (
         <Wrapper>
-            <button className='btn prev-btn'>
+            <button className='btn prev-btn' onClick={() => {
+                let prevPage = currentPage - 1;
+                if (prevPage < 1) prevPage = numOfPages;
+                handlePageChange(prevPage);
+            }}>
                 <HiChevronDoubleLeft />
                 Prev
             </button>
             <div className="btn-container">
-                {pages.map((pageNumber)=>{
-                    return <button className={`btn page-btn ${pageNumber===currentPage && 'active'}`} key={pageNumber}>{pageNumber}</button>
+                {pages.map((pageNumber) => {
+                    return <button className={`btn page-btn ${pageNumber === currentPage && 'active'}`} key={pageNumber}
+                        onClick={() => handlePageChange(pageNumber)}>
+                        {pageNumber}</button>
                 })}
             </div>
-            <button className='btn next-btn'>
+            <button className='btn next-btn' onClick={() => {
+                let nextPage = currentPage + 1;
+                if (nextPage > numOfPages) nextPage = 1;
+                handlePageChange(nextPage);
+            }}>
                 Next
                 <HiChevronDoubleRight />
 
